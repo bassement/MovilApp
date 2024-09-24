4ba9ff2eeacb9aa93d0c8e63b429e70abd6f4caf
Isaí Jeremías <conai2602018@gmail.com>
2024-09-24 15:11:36 -0300

    implementación habit_service

--- src/app/pages/habits/recommendations/recommendations.page.ts
+++ src/app/pages/habits/recommendations/recommendations.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { LoadingController } from '@ionic/angular';
+import { HabitService } from 'src/app/services/habit.service';
 
 @Component({
   selector: 'app-recommendations',
@@ -7,21 +8,29 @@ import { LoadingController } from '@ionic/angular';
   styleUrls: ['./recommendations.page.scss'],
 })
 export class RecommendationsPage {
-  constructor(private loadingController: LoadingController) { }
+  constructor(
+    private loadingController: LoadingController,
+    private habitService: HabitService
+  ) { }
 
   async presentLoading(event: any) {
     if (event.detail.checked) {
       const loading = await this.loadingController.create({
         message: 'Agregando',
-        duration: 400,
+        duration: 500,
         spinner: 'lines-sharp',
       });
 
       await loading.present();
+    }
+  }
 
-
-      const { role, data } = await loading.onDidDismiss();
-      console.log('Loading dismissed!');
+  onHabitChange(habit: string, event: any) {
+    if (event.detail.checked) {
+      this.habitService.addHabit(habit);
+      this.presentLoading(event);
+    } else {
+      this.habitService.removeHabit(habit);
     }
   }
 }
