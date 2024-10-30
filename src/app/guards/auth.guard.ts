cf36b74364fbfe19816e70873adb292e4044d286
Isaí Jeremías <conai2602018@gmail.com>
2024-10-30 15:04:06 -0300

    guard D pollito

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,21 +1,56 @@
-import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, GuardResult, MaybeAsync } from '@angular/router';
+import { inject, Injectable } from '@angular/core';
 import { FirebaseService } from '../services/firebase.service';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
 import { UtilsService } from '../services/utils.service';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  const firebaseSvc = inject(FirebaseService);
-  const utilsSvc = inject(UtilsService);
-  let user = localStorage.getItem('User');
 
-  return new Promise((resolve) => {
-    firebaseSvc.getAuth().onAuthStateChanged((auth) => {
-      if (auth) {
-        if (user) resolve(true); //Permitimos el acceso si es que el usuario esta autentificadco y si tmb existe en el localstorage
-      } else {
-        firebaseSvc.signOut(); // Nuestra funcionn signOut se encarga de borrar el item de local storage, redirigir al login y cerrar la sesion
-        resolve(false);
-      }
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class authGuard implements CanActivate {
+
+
+
+
+  firebaseSvc = inject(FirebaseService);
+  utilSvc = inject(UtilsService);
+
+
+
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+
+    let user = localStorage.getItem('user');
+
+    return new Promise((resolve) => {
+
+
+
+
+      this.firebaseSvc.getAuth().onAuthStateChanged((auth) => {
+
+        if (auth) {
+          if (user) resolve(true);
+
+
+
+        }
+        else {
+          this.utilSvc.routerLink('/auth');
+          resolve(false);
+        }
+      })
     });
-  });
-};
+  }
+
+
+}
--- src/app/guards/no-auth.guard.ts
+++ src/app/guards/no-auth.guard.ts
@@ -1,20 +1,54 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, GuardResult, MaybeAsync } from '@angular/router';
+import { inject, Injectable } from '@angular/core';
 import { FirebaseService } from '../services/firebase.service';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
 import { UtilsService } from '../services/utils.service';
-import { inject } from '@angular/core';
 
-export const noAuthGuard: CanActivateFn = (route, state) => {
-  const firebaseSvc = inject(FirebaseService);
-  const utilsSvc = inject(UtilsService);
 
-  return new Promise((resolve) => {
-    firebaseSvc.getAuth().onAuthStateChanged((auth) => {
-      if (!auth) resolve(true); // Permitimos el acceso al auth si no esta autentificado
-      else {
-        utilsSvc.routerLink('/main/habits');
-        console.log('Usuario autenticado, redirigirendo a Habits')
-        resolve(false); // Redirigimos al home si esta autentificado
-      }
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class noAuthGuard implements CanActivate {
+
+
+
+
+  firebaseSvc = inject(FirebaseService);
+  utilSvc = inject(UtilsService);
+
+
+
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+
+
+    return new Promise((resolve) => {
+
+
+
+
+      this.firebaseSvc.getAuth().onAuthStateChanged((auth) => {
+
+        if (!auth) resolve(true);
+
+
+
+
+        else {
+          this.utilSvc.routerLink('/habits');
+          resolve(false);
+        }
+      })
     });
-  });
-};
+  }
+
+
+}
